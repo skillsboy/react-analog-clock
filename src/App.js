3c0ef1116dc35e853257c5cdc5889f7debd4446e
skillsboy <milesmontgomery4@gmail.com>
2022-03-01 15:20:45 +0000

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const hourHand = useRef();
+	const minHand = useRef();
+	const secHand = useRef();
+	const [timestamp, setTimestamp] = useState("");
+
+	function runClock() {
+		const d = new Date(); //object of date()
+		const hr = d.getHours();
+		const min = d.getMinutes();
+		const sec = d.getSeconds();
+		const hr_rotation = 30 * hr + min / 2; //converting current time
+		const min_rotation = 6 * min;
+		const sec_rotation = 6 * sec;
+
+		setTimestamp(d);
+
+		hourHand.current.style.transform = `rotate(${hr_rotation}deg)`;
+		minHand.current.style.transform = `rotate(${min_rotation}deg)`;
+		secHand.current.style.transform = `rotate(${sec_rotation}deg)`;
+	}
+
+	useEffect(() => {
+		runClock();
+		setInterval(runClock, 300);
+	}, []);
+
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className="clock">
+					<span ref={hourHand} className="hour"></span>
+					<span ref={minHand} className="min"></span>
+					<span ref={secHand} className="sec"></span>
+				</div>
+				<p><code>{timestamp.toString()}</code></p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
